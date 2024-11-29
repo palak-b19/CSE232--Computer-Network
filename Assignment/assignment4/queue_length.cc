a93cecf934fb9cd78e2abd8c895cc3894a77f387
Palak Bhardwaj <119069053+palak-b19@users.noreply.github.com>
2024-11-29 17:56:28 +0530

    Update queue_length.cc
    file output

--- Assignment/assignment4/queue_length.cc
+++ Assignment/assignment4/queue_length.cc
@@ -5,19 +5,26 @@
 #include "ns3/applications-module.h"
 #include "ns3/flow-monitor-module.h"
 #include "ns3/queue.h"
+#include <fstream>
 
 using namespace ns3;
-
+std::ofstream logFile;
 // Function to log queue lengths
-void LogQueueLength(Ptr<Queue<Packet>> queue, std::string routerName, std::string linkName) {
+void LogQueueLength(Ptr<Queue<Packet>> queue, std::string linkDescription) {
     uint32_t queueLength = queue->GetNPackets();
-    std::cout << Simulator::Now().GetSeconds() << "s: " << routerName << " -> " << linkName
+    logFile << Simulator::Now().GetSeconds() << "s: " << linkDescription
               << " Queue Length: " << queueLength << " packets" << std::endl;
 }
 
 int main(int argc, char *argv[]) {
     CommandLine cmd;
     cmd.Parse(argc, argv);
+
+    logFile.open("queue_lengths.txt", std::ios::out);
+    if (!logFile.is_open()) {
+        std::cerr << "Error: Could not open log file!" << std::endl;
+        return 1;
+    }
 
     // Set up nodes
     NodeContainer hosts;
@@ -29,23 +36,54 @@ int main(int argc, char *argv[]) {
     stack.Install(hosts);
     stack.Install(routers);
 
-    // Set up point-to-point links
-    PointToPointHelper p2p;
-    p2p.SetChannelAttribute("Delay", StringValue("2ms"));
-    p2p.SetDeviceAttribute("DataRate", StringValue("1Mbps"));
-
-    // Host-to-router links
+    // Host-to-router links with different data rates
+    std::string hostToRouterRates[7] = {"1Mbps", "2Mbps", "1.5Mbps", "3Mbps", "1Mbps", "2Mbps", "2.5Mbps"};
     NetDeviceContainer hostDevices[7];
     for (uint32_t i = 0; i < 7; ++i) {
+        PointToPointHelper p2p;
+        p2p.SetChannelAttribute("Delay", StringValue("2ms"));
+        p2p.SetDeviceAttribute("DataRate", StringValue(hostToRouterRates[i]));
+
         hostDevices[i] = p2p.Install(NodeContainer(hosts.Get(i), routers.Get(i % 4)));
+
+        std::string hostToRouterLink =  "R" + std::to_string((i % 4) + 1) + " -> "  + " Host " + std::string(1, 'A' + i) ;
+        Ptr<NetDevice> device = hostDevices[i].Get(0); // Outgoing device at host
+        Ptr<PointToPointNetDevice> p2pDevice = DynamicCast<PointToPointNetDevice>(device);
+        if (p2pDevice) {
+            Ptr<Queue<Packet>> queue = p2pDevice->GetQueue();
+            Simulator::Schedule(Seconds(1.0), &LogQueueLength, queue, hostToRouterLink);
+            Simulator::Schedule(Seconds(2.0), &LogQueueLength, queue, hostToRouterLink);
+        }
     }
 
-    // Router-to-router links
+    // Router-to-router links with different data rates
+    std::string routerToRouterRates[4] = {"4Mbps", "5Mbps", "3.5Mbps", "4.5Mbps"};
     NetDeviceContainer routerDevices[4];
-    routerDevices[0] = p2p.Install(NodeContainer(routers.Get(0), routers.Get(1))); // R1 -> R2
-    routerDevices[1] = p2p.Install(NodeContainer(routers.Get(0), routers.Get(2))); // R1 -> R3
-    routerDevices[2] = p2p.Install(NodeContainer(routers.Get(2), routers.Get(3))); // R3 -> R4
-    routerDevices[3] = p2p.Install(NodeContainer(routers.Get(1), routers.Get(3))); // R2 -> R4
+    for (uint32_t i = 0; i < 4; ++i) {
+        PointToPointHelper p2p;
+        p2p.SetChannelAttribute("Delay", StringValue("2ms"));
+        p2p.SetDeviceAttribute("DataRate", StringValue(routerToRouterRates[i]));
+
+        if (i == 0) routerDevices[i] = p2p.Install(NodeContainer(routers.Get(0), routers.Get(1))); // R1 -> R2
+        else if (i == 1) routerDevices[i] = p2p.Install(NodeContainer(routers.Get(0), routers.Get(2))); // R1 -> R3
+        else if (i == 2) routerDevices[i] = p2p.Install(NodeContainer(routers.Get(2), routers.Get(3))); // R3 -> R4
+        else if (i == 3) routerDevices[i] = p2p.Install(NodeContainer(routers.Get(1), routers.Get(3))); // R2 -> R4
+
+        std::string linkDescription;
+        if (i == 0) linkDescription = "R1 -> R2";
+        else if (i == 1) linkDescription = "R1 -> R3";
+        else if (i == 2) linkDescription = "R3 -> R4";
+        else if (i == 3) linkDescription = "R2 -> R4";
+
+        Ptr<NetDevice> device = routerDevices[i].Get(0); // Outgoing device at router
+        Ptr<PointToPointNetDevice> p2pDevice = DynamicCast<PointToPointNetDevice>(device);
+        if (p2pDevice) {
+            Ptr<Queue<Packet>> queue = p2pDevice->GetQueue();
+            Simulator::Schedule(Seconds(1.0), &LogQueueLength, queue, linkDescription);
+            Simulator::Schedule(Seconds(2.0), &LogQueueLength, queue, linkDescription);
+            Simulator::Schedule(Seconds(3.0), &LogQueueLength, queue, linkDescription);
+        }
+    }
 
     // Install Internet stack
     Ipv4AddressHelper address;
@@ -59,22 +97,6 @@ int main(int argc, char *argv[]) {
 
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-    // Configure tracing for queue lengths
-    for (uint32_t i = 0; i < 4; ++i) {
-        Ptr<NetDevice> device = routerDevices[i].Get(0); // Outgoing device at router
-        Ptr<PointToPointNetDevice> p2pDevice = DynamicCast<PointToPointNetDevice>(device);
-        if (p2pDevice) {
-            Ptr<Queue<Packet>> queue = p2pDevice->GetQueue();
-            std::string routerName = "R" + std::to_string(i + 1);
-            std::string linkName = "Link " + std::to_string(i + 1);
-
-            Simulator::Schedule(Seconds(1.0), &LogQueueLength, queue, routerName, linkName);
-            Simulator::Schedule(Seconds(2.0), &LogQueueLength, queue, routerName, linkName);
-            Simulator::Schedule(Seconds(3.0), &LogQueueLength, queue, routerName, linkName);
-            // Add more schedule calls if needed
-        }
-    }
-
     // UDP Echo Server and Clients
     UdpEchoServerHelper echoServer(9);
     for (uint32_t i = 0; i < 7; ++i) {
@@ -83,13 +105,13 @@ int main(int argc, char *argv[]) {
 
     UdpEchoClientHelper echoClient(Ipv4Address("10.1.0.1"), 9);
     echoClient.SetAttribute("MaxPackets", UintegerValue(2000));
-    echoClient.SetAttribute("Interval", TimeValue(Seconds(0.0001)));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(0.01)));
     echoClient.SetAttribute("PacketSize", UintegerValue(2048));
     for (uint32_t i = 1; i < 7; ++i) {
         echoClient.Install(hosts.Get(i));
     }
 
-    Simulator::Stop(Seconds(10.0));
+    Simulator::Stop(Seconds(20.0));
     Simulator::Run();
     Simulator::Destroy();
 
